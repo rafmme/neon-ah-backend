4bc3c6b0a99248beda0e8dd269d77b2781d16778
Adeniran Samuel <adeniransamuel93@gmail.com>
2019-01-08 15:14:50 +0100

    Edit the test.js file to abide by eslint standard

--- test/sampleTest.js
+++ test/sampleTest.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 
-describe('test', ()=> {
-    it('Sample test', ()=> {
-        expect(true).to.equal(true);
-    });
-})
+describe('test', () => {
+  it('Sample test', () => {
+    expect(true).to.equal(true);
+  });
+});
