b51af645248f0c40b1c22dfd4a62042d22b0bc35
tejiri4 <tejirimatthew@Tejiris-MacBook-Pro.local>
2019-01-09 12:06:43 +0100

    chore(unit testing): set up unit testing
    - Describe test and assertion for unit test
    - Add exit and timeout to test script
    
    finishes: 162414264

--- test/sampleTest.js
+++ test/sampleTest.js
@@ -1,7 +1,12 @@
 import { expect } from 'chai';
+import server from '../index';
 
-describe('test', () => {
+describe('set up unit test', () => {
   it('Sample test', () => {
     expect(true).to.equal(true);
+  });
+  it('Should return neon', () => {
+    const team = 'neon';
+    expect(team).to.equal('neon');
   });
 });
